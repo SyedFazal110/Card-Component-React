8618929783d8e4bcbfa4c7bc362bb21f06e46f8d
SyedFazal110 <syedfazal808@gmail.com>
2024-09-04 01:20:07 +0500

    react cards

---
+++ card/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Cards from "./card";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Cards />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ card/src/card.jsx
@@ -0,0 +1,154 @@
+import React from "react";
+import "./index.css";
+
+function Cards() {
+  return (
+    <div className="cards-container">
+      <div className="card">
+        <img
+          src="https://t3.ftcdn.net/jpg/04/79/14/06/360_F_479140608_7xjG5HfKnYSDoEErrGYaSDIVzNPPxArw.jpg"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBxhc1Rct3XnJYWa1tighcktIVX7C5JyRLVA&s"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlek6u5nPp7B4hR8fxLGKdK8vkSIhabi8hYg&s"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/19cd86fb-51c7-4daa-b9a9-69ad8b6bc9f5/W+AIR+ZM+ALPHAFLY+NEXT%25+3+OLY.png"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw2rfUHzc_00deBiwNKueZZ88EByw1oqul9g&s"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://images.pexels.com/photos/1456706/pexels-photo-1456706.jpeg?cs=srgb&dl=pexels-craytive-1456706.jpg&fm=jpg"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbwiYRAGl-mCnvtV8DPTbwdJdYhKKxpBsfIX0Pi6qmiVsK1tVcu22EJisbmpzeI6bJhF0&usqp=CAU"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ84sQq73ijltQIfIoDBKg9bd6cDKUvF5Ex6_k94O4Mmi2IdC8Ze2jTS_XfxbUt3embQsA&usqp=CAU"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAzgjjrQEHizbRjfvroTLmdPINYeNXuacd1Q&s"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://c4.wallpaperflare.com/wallpaper/436/436/13/nike-air-force-hyperbeast-wallpaper-preview.jpg"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU0cr7SaYtsrYcCqoPmVF9pgcxpLEqfHIr4Q&s"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+
+      <div className="card">
+        <img
+          src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f43cf8dd-2f8b-4328-ab8b-d803c3df557f/W+AIR+ZOOM+PEGASUS+41.png"
+          className="img"
+          alt="Card Image"
+        />
+        <h2>Nike</h2>
+        <p>Price 20$</p>
+        <p>50% Discount offer</p>
+        <button className="card-btn">Learn More</button>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
